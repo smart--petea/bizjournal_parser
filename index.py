db25bfc1e7295b7c47562964ddcb87ab7ad3dc0e
petru <smart__petea@mail.ru>
2018-10-31 10:22:05 +0200

    init

---
+++ index.py
@@ -0,0 +1,41 @@
+import pymongo
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+mongoclient = pymongo.MongoClient("mongodb://localhost:27017/")
+bizdb = mongoclient['bizjournal']
+bizcol = bizdb['bizjournal']
+
+cursor = bizcol.find({}).sort("_id",  -1).limit(1);
+if cursor.count() == 0 :
+    page = 0
+else:
+    page = int(cursor[0]['page'])
+
+print page
+
+driver = webdriver.Chrome()
+
+while 1:
+    page = page + 1
+    print "Page %d \n\n" % page
+    driver.get('https://www.bizjournals.com/profiles/company?q=&s=2&pl=%d' % page);
+
+    els = driver.find_elements(By.XPATH, "//*[contains(@class, 'primary')]//a[contains(@class,'item')]")
+
+    for el in els:
+        print el.get_attribute('href')
+
+        toinsert = {
+                'page': page,
+                'href': el.get_attribute('href')
+        }
+        bizcol.insert_one(toinsert)
+
+    mongoclient.fsync()
+    break
+
+driver.close()
